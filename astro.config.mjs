da0ffb9cff4948be3a39d4f4eb7893a7a5ff6bdb
Saswati Choudhury <saswatichoudhury2227@gmail.com>
2024-12-11 13:32:10 +0530

    installation guide page added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,50 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'SignMate Docs',
-			social: {
-				github: 'https://github.com/saswati-26',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ 
-							label: 'Introduction', 
-							slug: 'introduction'
-						 },
-						 {
-							label: 'Getting Started',
-							slug: 'getting_started'
-						 },
-						 {
-							label: 'Features',
-							slug: 'features'
-						 },
-						 {
-							label: 'Admin Website',
-							slug: 'admin_website'
-						 },
-						 {
-							label: 'User Guide',
-							slug: 'user_guide'
-						 },
-						 {
-							label: 'FAQs',
-							slug: 'faqs'
-						 }
-					],
-				}
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "SignMate Docs",
+      social: {
+        github: "https://github.com/saswati-26",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: "Introduction",
+              slug: "introduction",
+            },
+            {
+              label: "Getting Started",
+              slug: "getting_started",
+            },
+            {
+              label: "Features",
+              slug: "features",
+            },
+            {
+              label: "Admin Website",
+              slug: "admin_website",
+            },
+            {
+              label: "User Guide",
+              slug: "user_guide",
+            },
+            {
+              label: "Installation Guide",
+              slug: "installation_guide",
+            },
+            {
+              label: "FAQs",
+              slug: "faqs",
+            },
+          ],
+        },
+      ],
+    }),
+  ],
 });
---
+++ src/content/docs/installation_guide.md
@@ -0,0 +1,40 @@
+---
+title: Installation Guide
+description: This website acts as installation guide for our SignMate application
+---
+
+## Installation Guide:
+
+This is the installation guide for the installation of **SignMate** mobile application. This contains the complete guide along with the respective screenshots for your convenience.
+
+<br />
+<br />
+
+**Step 1:** Click on the installation link provided in the [Getting Started - Installation](/getting_started#installation) based on your OS type (Android or iOS)
+![installation link](/src/assets/SignMate-removebg.png "Installation Link")
+
+<br />
+
+**Step 2:** After clicking on the link the app starts downloading. It will take some time depending upon your internet speed and will get downloaded.
+
+<br />
+
+**Step 3:** After the downloading is completed, now click on the `install` button and install the app.
+
+<br />
+
+**Step 4:** After installation click on `open` to open the app.
+
+<br />
+
+![link video](/src/assets/install_videos/VN20241211_124914.mp4)
+![install video](/src/assets/install_videos/VN20241211_125055.mp4)
+![open video](/src/assets/install_videos/VN20241211_125132.mp4)
+
+<br />
+Now, **SignMate** has successfully installed in your mobile phone.
+
+<br />
+<br />
+
+**_Happy Learning, Learners!!_**
